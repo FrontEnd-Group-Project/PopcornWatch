ab505a2dfb9c98ed597ecac9a63d9fb3cae0a7ed
dejaclarke <dejclark@cisco.com>
2023-07-07 13:17:41 -0700

    Nav bar

--- popcorn-watch/src/App.js
+++ popcorn-watch/src/App.js
@@ -1,29 +1,30 @@
 import "./App.css";
-import { Home } from './routes/Home'
-import { Shows } from './routes/Shows'
-import { 
-  createBrowserRouter,
-  RouterProvider,
-  } from 'react-router-dom';
+import { Upcoming } from "./routes/Upcoming";
+import { Home } from "./routes/Home";
+import { Shows } from "./routes/Shows";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home/>,
+    path: "/",
+    element: <Home />,
   },
   {
-    path: '/shows',
-    element: <Shows/>
-  }
-])
+    path: "/shows",
+    element: <Shows />,
+  },
+  {
+    path: "/upcoming",
+    element: <Upcoming />,
+  },
+]);
 
 function App() {
   return (
-    <div className='mt-4 mx-auto h-75 w-75'>
-      <RouterProvider router={router}/>
+    <div className="mt-4 mx-auto h-75 w-75">
+      <RouterProvider router={router} />
     </div>
   );
 }
 
-
-export {App};
+export { App };
