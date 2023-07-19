679601af9705f73805f1233d08018abdc4ac6fc3
Addy Rodriguez <rdz.33.a@gmail.com>
2023-07-19 15:16:20 -0500

    Added search to navbar

--- popcorn-watch/src/components/Navigation.js
+++ popcorn-watch/src/components/Navigation.js
@@ -2,14 +2,21 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import { Form, FormControl, Button } from "react-bootstrap";
+
 
 function Navigation() {
   return (
-    <Navbar bg="black" variant="dark">
+    <Navbar bg="black" expand="lg" variant="dark">
       <Container fluid>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/shows">Shows</NavLink>
         <NavLink to="/upcoming">Upcoming</NavLink>
+        <Form className="d-flex">
+          <FormControl type="search" placeholder="Search" className="me-2" aria-label="search" name=""> 
+          </FormControl>
+          <Button variant="dark">Search</Button>
+        </Form>
       </Container>
     </Navbar>
   );
